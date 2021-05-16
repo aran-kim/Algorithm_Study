ebe8102f3ee72c9d1611085a877481c4591fb4f9
aran-kim <aran0317@naver.com>
2021-05-17 00:02:34 +0900

    05.16

--- dynamic programming/dynamic programming/백준9184_신나는 함수 실행.cpp
+++ dynamic programming/dynamic programming/백준9184_신나는 함수 실행.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int dp = 0;
+int dp[101][101][101] = { 0, };
+
 int w(int a, int b, int c) {
-	if (a <= 0 || b <= 0 || c <= 0) {
-		return 1;
+	if (dp[a + 50][b + 50][c + 50] != 0) {
+		return dp[a + 50][b + 50][c + 50];
+	}
+	else if (a <= 0 || b <= 0 || c <= 0) {
+		return dp[a+50][b+50][c+50] = 1;
 	}
 	else if (a > 20 || b > 20 || c > 20) {
-		return w(20, 20, 20);
+		return dp[a + 50][b + 50][c + 50] = w(20, 20, 20);
 	}
 	else if (a < b && b < c) {
-		return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+		return dp[a + 50][b + 50][c + 50] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
 	}
 	else {
-		return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+		return dp[a + 50][b + 50][c + 50] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
 	}
 }
 
@@ -22,11 +26,9 @@ int main() {
 	ios::sync_with_stdio(0);
 	int a, b, c;
 	cin >> a >> b >> c;
-	while (a != -1 && b != -1 && c != -1) {
-		dp = w(a, b, c);
-		printf("w(%d, %d, %d) = %d\n", a, b, c, dp);
+	while (a != -1 || b != -1 || c != -1) {
+		w(a, b, c);
+		printf("w(%d, %d, %d) = %d\n", a, b, c, dp[a+50][b+50][c+50]);
 		cin >> a >> b >> c;
 	}
 }
-
-//¤¡¤§
