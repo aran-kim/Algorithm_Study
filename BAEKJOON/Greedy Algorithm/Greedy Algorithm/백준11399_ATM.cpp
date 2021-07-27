dd75a54a119b91efce92e61f11dc6b16412014d2
aran-kim <aran0317@naver.com>
2021-07-27 22:59:17 +0900

    07.27

---
+++ BAEKJOON/Greedy Algorithm/Greedy Algorithm/백준11399_ATM.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int compare(int a, int b) {
+	return a < b;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	vector<int> P(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> P[i];
+	}
+
+	sort(P.begin(), P.end(), compare);
+	
+	int sum = P[0];
+	int temp = P[0];
+	for (int i = 1; i < N; i++) {
+		temp += P[i];
+		cout << sum << P[i] << "\n";
+		sum += temp;
+	}
+
+	cout << sum;
+
+}
